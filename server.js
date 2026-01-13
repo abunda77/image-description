4a2cd04239e1a7b275b73413259d79fb3ee07f06
Erie Putranto <erieputranto@gmail.com>
2026-01-13 09:43:08 +0700

    fix: IP 2

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/api/health', (req, res) => {
 });
 
 // Serve index.html for all other routes (SPA fallback)
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
